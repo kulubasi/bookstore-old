d7c761367695c634d0342dea91719daf4be2f193
kulubasi <61836873+kulubasi@users.noreply.github.com>
2023-05-01 07:59:42 +0300

    updated app page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+let counter=0;
 function App() {
+  counter++;
   return (
     <div className="App">
-      <h2> Welcome to bookstore</h2>
+      <h2> Welcome to bookstore there are {counter} books here</h2>
       <h3> Easily Manage books </h3>
     </div>
   );
 }
-
+setInterval(App, 2000);
 export default App;
